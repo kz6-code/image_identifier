bc5f0bcf14bcb09bdd3c9d17ccbc48c903806aad
kz6-code <80061504+kz6-code@users.noreply.github.com>
2022-06-03 15:50:59 +0100

    Add files via upload

---
+++ Image_recognizer/main.js
@@ -0,0 +1,47 @@
+      
+Webcam.attach( '#camera' );
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+camera = document.getElementById("camera");
+      
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/lRQsQpzzU/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+// A function to run when we get any errors and the results
+function gotResult(error, results) {
+  // Display error in the console
+  if (error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
